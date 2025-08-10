ad9fe793eefcc25dbc6b67d363a17e5d614774a2
Mahak5123 <mahaksalecha61@gmail.com>
2025-08-10 22:51:21 +0530

    Updated files

--- client/src/components/Slide/Slide.jsx
+++ client/src/components/Slide/Slide.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import './slide.scss';
-import { Slider } from "infinite-react-carousel";
+import Slider from "react-slick";  // default import for react-slick
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
-const Slide = ({children,slidesToShow, arrowsScroll}) => {
-    return ([
-        <div className="slide">
-            <div className="container">
-                <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
-                    {children}
-                </Slider>
-            </div>
-        </div>
-    ]);
-}
+const Slide = ({ children, slidesToShow, arrowsScroll }) => {
+  // react-slick does not have arrowsScroll, instead it has slidesToScroll
+  const settings = {
+    slidesToShow: slidesToShow || 1,
+    slidesToScroll: arrowsScroll || 1,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    arrows: true, // enables arrows
+  };
+
+  return (
+    <div className="slide">
+      <div className="container">
+        <Slider {...settings}>
+          {children}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
 export default Slide;
